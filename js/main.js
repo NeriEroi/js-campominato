86efe19797a825a46e7565cbeed810d4b8aa6d4f
Michele <micheleierniero@gmail.com>
2021-07-26 14:16:38 +0200

    16 n casuali, seconda soluzione

--- js/main.js
+++ js/main.js
@@ -1,22 +1,46 @@
 // Il computer deve generare 16 numeri casuali (le nostre bombe) tra 1 e 100.
 
-function mine() {
-    n = 16;
-    if (n > 0 && n <= 100) {
-      str = "";
-      for (i = 0; i < n; i++) {
-           num = Math.round(Math.random() * 100 + 1);
-           if (i > 0) {
-              str = str + ", ";
-           }
-           str = str + num;
-        }
-        document.getElementById("casuali").innerHTML = "Mine: " + str;
-    } 
-    else {
-        document.getElementById("casuali").innerHTML = "Inserisci un numero maggiore di 0 e inferiore a 100";
+// function mine() {
+//     n = 16;
+//     if (n > 0 && n <= 100) {
+//       str = "";
+//       for (i = 0; i < n; i++) {
+//            num = Math.round(Math.random() * 100 + 1);
+//            if (i > 0) {
+//               str = str + ", ";
+//            }
+//            str = str + num;
+//         }
+//         document.getElementById("casuali").innerHTML = "Mine: " + str;
+//     } 
+//     else {
+//         document.getElementById("casuali").innerHTML = "Inserisci un numero maggiore di 0 e inferiore a 100";
+//     }
+// }
+
+
+var arrayMine = [];
+
+var mine = mineGenerate(1, 101);
+
+console.log(arrayMine);
+
+function mineGenerate (min, max) {
+    for (i = 0; i < 16; i++) {
+        arrayMine.push(Math.round(Math.random() * (max - min)) + min);
     }
+    return Math.round(Math.random() * (max - min)) + min;
 }
+
+
+
+
+
+
+
+
+
+
 
 
   
