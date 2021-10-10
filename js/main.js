ebc8195ae997d8e2df3e12c3b6eb9025b5b219ca
Michele <micheleierniero@gmail.com>
2021-10-10 02:40:28 +0200

    versione del campominato alternativa

--- js/main.js
+++ js/main.js
@@ -38,79 +38,125 @@
 
 
 
-function randomNumbers (min, max){
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
+// function randomNumbers (min, max){
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// }
 
-// difficoltà
-var level = parseInt(prompt("Scegli il livello di difficoltà: [0/1/2]"));
-var limit;
-switch (level) {
+// // difficoltà
+// var level = parseInt(prompt("Scegli il livello di difficoltà: [0/1/2]"));
+// var limit;
+// switch (level) {
 
-  case 1:
+//   case 1:
       
-      limit = 80;
-      break;
+//       limit = 80;
+//       break;
 
-  case 2:
+//   case 2:
      
-      limit = 50;
-      break;
+//       limit = 50;
+//       break;
 
-  default:
+//   default:
      
-      limit = 100;
-      break;
-}
-console.log(limit);
+//       limit = 100;
+//       break;
+// }
+// console.log(limit);
 
-// array da 16 numeri generati casualmente
-var bombs = [];
+// // array da 16 numeri generati casualmente
+// var bombs = [];
 
 
-while (bombs.length < 16){
-  var pcNumber = randomNumbers(1, limit)
+// while (bombs.length < 16){
+//   var pcNumber = randomNumbers(1, limit)
   
-  if (!bombs.includes(pcNumber)){
-      bombs.push(pcNumber)
-  }
+//   if (!bombs.includes(pcNumber)){
+//       bombs.push(pcNumber)
+//   }
 
-}
-console.log(bombs);
-
-
-var userArray = [];
+// }
+// console.log(bombs);
 
 
-for (i = 1; i <= (limit - 16); i ++){
-  var userNumber = parseInt(prompt("Inserisci un numero tra uno e " + limit + "."))
+// var userArray = [];
+
+
+// for (i = 1; i <= (limit - 16); i ++){
+//   var userNumber = parseInt(prompt("Inserisci un numero tra uno e " + limit + "."))
   
  
-  while (userNumber < 1 || userNumber > limit || isNaN(userNumber)){
-      alert("Inserisci un numero valido!")
-      userNumber = parseInt(prompt("Inserisci un numero tra uno e " + limit + "."))
-  }
+//   while (userNumber < 1 || userNumber > limit || isNaN(userNumber)){
+//       alert("Inserisci un numero valido!")
+//       userNumber = parseInt(prompt("Inserisci un numero tra uno e " + limit + "."))
+//   }
 
   
-  if (!userArray.includes(userNumber)){
-      if(bombs.includes(userNumber)){
-          alert("Bomba! Hai perso! Il tuo punteggio è:" + userArray.length);
-          break;
-      }
-      userArray.push(userNumber);
-      console.log(userArray);
+//   if (!userArray.includes(userNumber)){
+//       if(bombs.includes(userNumber)){
+//           alert("Bomba! Hai perso! Il tuo punteggio è:" + userArray.length);
+//           break;
+//       }
+//       userArray.push(userNumber);
+//       console.log(userArray);
 
-  } else {
-      alert("Numero già inserito!");
-      i--;
-  }
+//   } else {
+//       alert("Numero già inserito!");
+//       i--;
+//   }
 
-}
+// }
 
 
-if(userArray.length === (limit - 16)) {
-  alert("Hai vinto! Punteggio:" + userArray.length);
-}
+// if(userArray.length === (limit - 16)) {
+//   alert("Hai vinto! Punteggio:" + userArray.length);
+// }
+
+// generazione bombe
+const array_bomb = [];
+const bombs_number = 16;
+let array_giocate = [];
+
+for(i = 0; i < bombs_number; i++) {
+    random_bomb = Math.floor(Math.random() * 100) + 1;
+    if (array_bomb.indexOf(random_bomb) == -1) {
+        array_bomb.push(random_bomb);
+    } else {
+        random_bomb;
+        array_bomb.push(random_bomb);
+    }
+};
+
+console.log(array_bomb);
+
+// richiesta delle giocate e verifica se il numero inserito coincida con una bomba oppure no
+let giocate = 3;
+
+
+for(i = 0; i < giocate; i++) {
+    playing_request = parseInt(prompt("Inserisci un numero e spera che non coincida con una bomba :)"));
+    if (array_bomb.includes(playing_request)) {
+        console.log("BOOOM!!! Hai perso!");
+        console.log("Numero di giocate utili: " + array_giocate.length);
+        break;
+    } else {
+        if (array_giocate.indexOf(playing_request) === -1) {
+            alert("Pfiuuuh!!! Sei ancora vivo!");
+            array_giocate.push(playing_request);
+        } else {
+            alert("Non si possono inserire per due volte gli stessi numeri!");
+            i-- //per fare in modo che non si conteggi il ciclo in cui è stato inserito lo stesso numero per due volte
+            playing_request;
+            if (array_giocate.indexOf(playing_request) === -1) {
+                alert("Pfiuuuh!!! Sei ancora vivo!");
+                array_giocate.push(playing_request);
+            }
+        }
+        if (array_giocate.length == giocate) {
+            console.log("Sììììì!!! Sei rimasto vivo e quindi HAI VINTO!");
+        }
+    }
+};
 
 
 
